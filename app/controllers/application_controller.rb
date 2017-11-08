561414f5e6695a1caef8ca12a7b1600c4a4165e4
Rodrigo Lodeiro <rodrigo.lodeiro@live.com>
2017-11-08 16:33:34 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,10 +30,10 @@ end
 	end
 
 	post "/login" do
-		user = User.find_by(:username => param[:username])
+		user = User.find_by(:username => params[:username])
 		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
- 			redirect '/success'
+			redirect "/success"
 		else
 			redirect '/failure'
 		end
